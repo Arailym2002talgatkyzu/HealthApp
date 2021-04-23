ccb372695621273889bc0a6243fad61a5fa94fcb
Arailym2002talgatkyzu <arailym_talgatkyzy_2002@mail.ru>
2021-04-23 10:23:36 +0600

    Adding the Speech

--- app/src/main/java/com/example/healthapp/ExerciseActivity.kt
+++ app/src/main/java/com/example/healthapp/ExerciseActivity.kt
@@ -5,21 +5,33 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.speech.tts.TextToSpeech
 import android.view.ActionMode
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_exercise.*
+import java.util.*
+
 class ExerciseActivity: AppCompatActivity() {
     private lateinit var player: MediaPlayer
     private lateinit var start: MediaPlayer
     private var restTimer: CountDownTimer?= null
     private var restProgress = 0
-    private var exerciseno=0
+    private var exerciseno=9
+    private var toSpeak=""
+    lateinit var mTTS: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
         setSupportActionBar(toolbar_exercise_activity)
+        mTTS = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
+            if (status != TextToSpeech.ERROR){
+                //if there is no error then set language
+                mTTS.language = Locale.UK
+            }
+        })
+
         player = MediaPlayer.create(applicationContext, R.raw.press_start)
         player!!.isLooping = false
         start = MediaPlayer.create(applicationContext, R.raw.beep)
@@ -52,13 +64,30 @@ class ExerciseActivity: AppCompatActivity() {
             }
 
             override fun onFinish() {
-                start!!.start()
-                exerciseno++
-                Toast.makeText(this@ExerciseActivity, "Start Exercising!",Toast.LENGTH_SHORT).show()
-                task.text="Exercise $exerciseno"
+               /* start!!.start()*/
 
-                status.text="30"
-                progress_circular.max=30
+                exerciseno++
+                toSpeak = when (exerciseno) {
+                    1 -> "Abdominal crunch"
+                    2-> "High knees running in place"
+                    3-> "Jumping jacks"
+                    4-> "Lunge"
+                    5-> "Plank"
+                    6-> "Push up"
+                    7-> "Push up and rotation"
+                    8-> "Side plank"
+                    9-> "Squat"
+                    10-> "Step up onto chair"
+                    11-> "Ic triceps dip on chair"
+                    12-> "Wall sit"
+                    else -> "Congratulations"
+                }
+                mTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null)
+                Toast.makeText(this@ExerciseActivity, "Start Exercising!",Toast.LENGTH_SHORT).show()
+                task.text="Exercise $exerciseno \n $toSpeak"
+
+                status.text="15"
+                progress_circular.max=15
                 when(exerciseno){
                     1-> exercise.setImageResource(R.drawable.ic_abdominal_crunch)
                     2-> exercise.setImageResource(R.drawable.ic_high_knees_running_in_place)
@@ -72,8 +101,6 @@ class ExerciseActivity: AppCompatActivity() {
                     10-> exercise.setImageResource(R.drawable.ic_step_up_onto_chair)
                     11-> exercise.setImageResource(R.drawable.ic_triceps_dip_on_chair)
                     12-> exercise.setImageResource(R.drawable.ic_wall_sit)
-
-
                 }
                   if(exerciseno<13){
                       setupActionView()
@@ -107,11 +134,11 @@ class ExerciseActivity: AppCompatActivity() {
 
     private fun setActionProgressBar(){
         progress_circular.progress = restProgress
-        restTimer = object : CountDownTimer(30000,1000){
+        restTimer = object : CountDownTimer(15000,1000){
             override fun onTick(millisUntilFinished: Long) {
                 restProgress ++
-                progress_circular.progress = 30 - restProgress
-                status.text = (30 - restProgress).toString()
+                progress_circular.progress = 15 - restProgress
+                status.text = (15 - restProgress).toString()
             }
 
             override fun onFinish() {
@@ -132,6 +159,7 @@ class ExerciseActivity: AppCompatActivity() {
         progress_circular.max=0
         status.text="0"
         task.text="CONGRATULATIONS!"
+        mTTS.speak("congratulations", TextToSpeech.QUEUE_FLUSH, null)
     }
 
 
